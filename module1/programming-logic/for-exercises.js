79bbe972f08dd94d9e917230cd8e5fe301675750
johel <johel.carvalho@gmail.com>
2021-01-30 11:35:41 -0300

    mod1-pt1: for problems exercises

---
+++ module1/programming-logic/for-exercises.js
@@ -0,0 +1,52 @@
+// Problema 1: Criar uma função que imprime os números de 1 a 50
+
+// Problema 2: Criar uma função que retorna a soma dos numeros de 1 a 50
+
+// Problema 3: Criar uma função que retorna a soma dos
+// numeros pares de 1 a 50
+
+// Problema 4: Criar a função print que imŕime os elementos de uma lista,
+
+// Problema 5: Criar a função sum que soma os elementos de uma lista,
+// ex: sum([1,4,5]) => 10
+
+// Problema 5: Criar a função multiply que multiplica os elementos de uma lista,
+// ex: sum([1,4,5]) => 20
+
+// Problema 5: Criar a função que soma os elementos de uma lista
+// maiores que 100 ou negativos;
+// ex: sum([100, -1, 10, 101, 0]) => 100
+
+// Problema 6: Criar a função que retorna uma outra lista, mas somente
+// dos elementos maiores que 10;
+// ex: filterGreaterThan10([100, -1, 10, 10.5, 0]) => [100, 10.5]
+
+// Problema 7: Criar uma função que retorna o maior elemento de uma lista
+// ex: maxFromList([10, -1, 80, 91, 11]) => 91
+
+// Problema 8: Criar uma função que retorna o maior elemento PAR de uma lista
+// ex: maxFromList([10, -1, 80, 91, 11]) => 80
+
+// Problema 9: Criar uma função que retorna outra lista, mas dobrando o valor
+// de cada elemento
+// ex: multiplyByTwoMap([1,2,3,4]) => [2,4,6,8]
+
+// Problema 10: Criar uma função que retorna um objeto que conta
+// a quantidade de números pares e ímpares na lista
+// ex: countEvenAndOddQuantities([1,2,3,4,5,5]) =>  {evenQuantity: 2, oddQuantity: 4}
+
+// Problema 11: Criar uma função que retorna um objeto que conta
+// a soma dos elementos ímpares e a soma do elementos pares
+// ex: countEvenAndOddQuantities([1,2,3,4,5,5]) =>  {evenSum: 2, oddSum: 4}
+
+// Problema 12: Criar uma função que verifica se um número é primo
+// ex: ehPrimo(17) =>  true
+// ex: ehPrimo(9) =>  false
+// ex: ehPrimo(2) =>  true
+// ex: ehPrimo(4) =>  false
+// Dica: Um número primo significa que ele é apenas divisível por 1 e por ele mesmo.
+// O número 7, por exemplo é primo, pois se percorrermos todos os números de 1 a 7,
+// apenas 7%1 e 7%7 serão zeros.
+// Todos os demais entre 2 e 7-1:  7%2, 7%3, 7%4, 7%5 e 7%6, não serão zero.
+
+// MAIS TARDE - PROBLEMAS COM LISTA DE OBJETOS
