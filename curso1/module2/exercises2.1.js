4cd773019752830b4fd2667f4365938b7279c5b9
johel <johel.carvalho@gmail.com>
2021-03-29 22:30:32 -0300

    feat: for and while exercises

---
+++ curso1/module2/exercises2.1.js
@@ -0,0 +1,60 @@
+// 1. Criar uma função que imprime os numeros de 1 a n,
+// onde n é um inteiro positivo;
+
+// 2. Criar uma função que imprime os numeros de n a 1,
+// onde n é um inteiro positivo;
+
+// 3. Criar uma função que imprime os pares de 1 a 50. Use o while
+// Nota: Não usar o if nem o else
+
+// 4. Criar uma função que imprime os ímpares de 1 a 50.
+// Nota: Não usar o if nem o else
+
+// 5. Criar uma função que retorna a soma dos numeros naturais de 1 a n.
+
+// 6. Criar uma função que retorna a soma dos numeros pares
+// naturais de 1 a n. Não usar o if else;
+
+// 7. Criar uma função que retorna a quantidade de numeros
+// de um numero inteiro positivo qualquer.
+
+// 8. Criar uma função que retorna a quantidade de caracteres
+// de um texto qualquer.
+
+// 9. Criar uma função que retorna o primeiro dígito
+// de um numero inteiro positivo qualquer.
+
+// 10. Criar uma função que calcula a potência k de um numero n
+// inteiro positivo qualquer. Use um loop.
+
+// 11. Criar uma função que calcula o fatorial de um numero.
+// Ex: 5!(5 fatorial) = 5 * 4 * 3 * 2 *1
+
+// 12. Criar uma função que verifica se um número natural é primo ou não.
+
+// 13. Criar uma função que reverte uma palavra. Usar o for.
+// ex: revertWord("texto") => "otxet"
+
+// 14. Criar uma função que reverte um numero.
+// Dica: transformar o numero em string, reverter e depois passar p/numero
+
+// 15. Criar uma função que calcula o mínimo múltiplo comum de dois
+// inteiros positivos.
+// ex: mmc(2,3) => 6;  mmc(4,12) => 12
+
+// 16. Criar uma função que imprime todos os numeros de um array.
+
+// 17. Criar uma função que imprime todos os numeros positivos um array.
+
+// 18. Criar uma função que imprime todos os elementos de um array
+// que estão posicionados em índices múltiplos de 5.
+// imprime5(["hey",2,3,6,7,8,9,0,"olá"]) => imprime hey e 8
+
+// 19. Criar uma função que imprime todos os elementos de um array
+// até encontrar um elemento repetido. Neste momento, para de imprimir.
+// ex: imprimeAteRepetido([1,2,3,4,4,5,6,7]) => imprime 1,2,3,4
+// ex: imprimeAteRepetido(["ho", "ho"]) => imprime "ho"
+
+// 20. Criar uma função que imprime todos os numeros contidos
+// em uma lista de lista de numeros.
+// ex: flatPrint([[4,5,6], [4,5,7], [14,25,6]]) => imprime 4,5,6,4,5,7,14,25,6
